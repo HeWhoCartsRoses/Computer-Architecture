d8c45e9335a9bd4a442ec1e1ce6862e45d77b075
HeWhoCartsRoses <carteradrewrose@gmail.com>
2020-07-21 14:40:37 -0600

    finished day 1

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,13 +1,20 @@
 """CPU functionality."""
-
 import sys
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0]*256
+        self.reg = [0]*8
+        self.pc = self.reg[0]
+        self.cmn = {
+            0b00000001: self.hlt,
+            0b10000010: self.ldi,
+            0b01000111: self.prn
+        }
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +25,35 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def hlt(self, a, b):
+        return (0, False)
+
+    def ldi(self, a, b):
+        self.reg[a] = b
+        return (3, True)
+
+    def prn(self, a, b):
+        print(self.reg[a])
+        return (2, True)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +65,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +77,27 @@ class CPU:
 
         print()
 
+    def ram_read(self, point):
+        return self.ram[point]
+
+    def ram_write(self, data, point):
+        self.ram[point] = data
+
     def run(self):
         """Run the CPU."""
-        pass
+        run = True
+
+        while run:
+            IR = self.ram[self.pc]
+
+            a = self.ram_read(self.pc + 1)
+            b = self.ram_read(self.pc + 2)
+
+            try:
+                op = self.cmn[IR](a, b)
+                run = op[1]
+                self.pc += op[0]
+
+            except:
+                print(IR)
+                break
